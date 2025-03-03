3aa9fd64c1c4cd41e76096673d665975e7289dec
Antonio <230642@utxicotepec.edu.mx>
2025-03-03 15:32:06 -0600

    Angular
    Practica-Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentComponent } from './components/content/content.component';
+
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [NavbarComponent,SidebarComponent,ContentComponent,BreadcrumbComponent,FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Angular_230642';
+}
